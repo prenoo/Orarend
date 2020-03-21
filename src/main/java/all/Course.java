4c6251be544e0c8349a0a76e4c2ef9beb380f6b5
Preno <prenomail@gmail.com>
2020-03-21 09:58:27 +0100

    Reading from a .doc file line by line

---
+++ src/main/java/all/Course.java
@@ -0,0 +1,27 @@
+package all;
+
+public class Course {
+
+    private enum Day {
+        HETFO,
+        KEDD,
+        SZERDA,
+        CSUTORTOK,
+        PENTEK,
+        SZOMBAT
+    }
+
+    private int felev;
+    private String kar;
+    private String szki;
+    private String ti;
+    private String tantargy;
+    private String tsz;
+    private String eloado;
+    private String csop;
+    private int fo;
+    private int kezd;
+    private int tart;
+    private String terem;
+    private Day nap;
+}
